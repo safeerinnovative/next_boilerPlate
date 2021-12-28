1b1a72ed1b06d9d161727e3e6af63a9891d33e6d
safeer-innovative <safeer@innovative-net.com>
2021-12-29 03:23:00 +0500

    dynamic route with dynamic data

--- pages/blog/[pageNo].js
+++ pages/blog/[pageNo].js
@@ -1,10 +1,31 @@
 import { useRouter } from "next/router"
 import Head from 'next/Head'
+import axios from 'axios';
+import { useEffect,useState } from 'react';
 
 export default function pageNo() {
 
+    const [CurrentData, setCurrentData] = useState([])
+    const [CurrentCode, setCurrentCode] = useState(String.empty)
+
     const router = useRouter();
     const pageNumber = router.query.pageNo;
+
+    useEffect(() => {
+        setCurrentCode(pageNumber)
+        if(pageNumber){
+            setCurrentDataToState()
+        }
+    }, [pageNumber])
+
+    const setCurrentDataToState = async () => {
+        let dummyData = await axios.get(`https://jsonplaceholder.typicode.com/todos/${pageNumber}`)
+        setCurrentData(dummyData.data)
+    }
+
+    setTimeout(() => {
+        console.log(CurrentData)
+    }, 5000);
 
     return (
         <section>
@@ -14,7 +35,8 @@ export default function pageNo() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div>
-                <h1>My Blog {pageNumber} Page</h1>
+                <h1>My Blog {CurrentData.id} Page</h1>
+                <p>{CurrentData.title}</p>
             </div>
         </section>
     )
