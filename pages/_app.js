3e6399048a81d51b9bd943c15bdee0170251f585
safeer-innovative <safeer@innovative-net.com>
2022-01-10 21:50:33 +0500

    added Next Progress Bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <NextNProgress
+      color="#61dbfb"
+      stopDelayMs={200}
+      height={5}
+    />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
